b2f515417f8a8320fca82ea6a2b230c73d74da3c
RonnyK36 <ronnykelvyne3@gmail.com>
2022-09-04 20:48:31 +0300

    Completed Basic todo app UI and added functionality to display todo list on form submit

---
+++ app.js
@@ -0,0 +1,17 @@
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const todoList = document.getElementById('todos')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const todo = input.value
+
+    if (todo) {
+        const todoEl = document.createElement('li')
+        todoEl.innerText = todo
+
+        todoList.appendChild(todoEl)
+
+        input.value = ''
+    }
+})
